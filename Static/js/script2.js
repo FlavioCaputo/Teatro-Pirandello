05137de975aa9acb21580e634fd52c4b4646427d
FlavioCaputo <161652421+FlavioCaputo@users.noreply.github.com>
2024-05-08 21:47:32 +0200

    Add files via upload

--- Static/js/script2.js
+++ Static/js/script2.js
@@ -1,11 +1,34 @@
-const panoramaImage = new PANOLENS.ImagePanorama('foyer.jpg');
-const imageContainer = document.getElementById('panorama');
+pannellum.viewer('panorama', {
+  "type": "equirectangular",
+  "panorama": "https://i.ibb.co/PrwmLHs/foyer.jpg",
+  "autoLoad": true,
+  "showControls": false,
+  "haov": 180,
+  "hotSpots": [
+    {
+        "pitch": 22,
+        "yaw": -45,
+        "type": "info",
+        "text": "Vincenzo Bellini",
+        "createTooltipFunc": hotspotClick,
+    },
+]
+})
+function hotspotClick(hotSpotDiv, args) {
+  hotSpotDiv.classList.add('custom-hotspot'); // Aggiungi una classe al hotspot per identificarlo
+  var nuovoOggetto = document.createElement('div');
+  nuovoOggetto.textContent = 'Vincenzo Bellini';
+  nuovoOggetto.classList.add('info'); 
+  hotSpotDiv.appendChild(nuovoOggetto);
 
-const viewer = new PANOLENS.Viewer({
-  container: imageContainer,
-  autoRotate: true,
-  autoRotateSpeed: 0.3,
-  controlBar: false,
-});
-
-viewer.add(panoramaImage);
+  hotSpotDiv.text = "Ciao";
+  hotSpotDiv.onclick = function() {
+    window.location.href = "../page/Vincenzo.html";
+  };
+  hotSpotDiv.onmouseenter = function() {
+    nuovoOggetto.style.display = "Block";
+  }
+  hotSpotDiv.onmouseleave = function() {
+    nuovoOggetto.style.display = "none";
+  }
+}

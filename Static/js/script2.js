5006f8b9741e11209d9555bdb1911c8eed7218f5
FlavioCaputo <161652421+FlavioCaputo@users.noreply.github.com>
2024-05-15 22:21:37 +0200

    Add files via upload

--- Static/js/script2.js
+++ Static/js/script2.js
@@ -6,24 +6,65 @@ pannellum.viewer('panorama', {
   "haov": 180,
   "hotSpots": [
     {
-        "pitch": 22,
-        "yaw": -45,
+        "pitch": 21,
+        "yaw": -46,
         "type": "info",
         "text": "Vincenzo Bellini",
         "createTooltipFunc": hotspotClick,
     },
+    {
+      "pitch": -7,
+      "yaw": 57,
+      "cssClass": "custom-hotspot2",
+      "text": "Stanza Laterale",
+      "createTooltipFunc": hotspot2,
+      "createTooltipArgs": "Custom Hotspot"
+    },
+    {
+      "pitch": 19,
+      "yaw": -72.5,
+      "type": "info",
+      "text": "Vincenzo Bellini",
+      "createTooltipFunc": hotspotClick2,
+   },
 ]
 })
 function hotspotClick(hotSpotDiv, args) {
   hotSpotDiv.classList.add('custom-hotspot'); // Aggiungi una classe al hotspot per identificarlo
   var nuovoOggetto = document.createElement('div');
-  nuovoOggetto.textContent = 'Vincenzo Bellini';
+  nuovoOggetto.textContent = 'Vittorio Alfieri';
   nuovoOggetto.classList.add('info'); 
   hotSpotDiv.appendChild(nuovoOggetto);
 
   hotSpotDiv.text = "Ciao";
   hotSpotDiv.onclick = function() {
-    window.location.href = "../page/Vincenzo.html";
+    window.location.href = "../page/Vittorio.html";
+  };
+  hotSpotDiv.onmouseenter = function() {
+    nuovoOggetto.style.display = "Block";
+  }
+  hotSpotDiv.onmouseleave = function() {
+    nuovoOggetto.style.display = "none";
+  }
+}
+
+function hotspot2(hotSpotDiv, args) {
+  hotSpotDiv.classList.add('custom-hotspot');
+  hotSpotDiv.onclick = function() {
+      window.location.href = "../page/Visita2.html";
+    };
+}
+
+function hotspotClick2(hotSpotDiv, args) {
+  hotSpotDiv.classList.add('custom-hotspot'); // Aggiungi una classe al hotspot per identificarlo
+  var nuovoOggetto = document.createElement('div');
+  nuovoOggetto.textContent = 'Carlo Goldoni';
+  nuovoOggetto.classList.add('info'); 
+  hotSpotDiv.appendChild(nuovoOggetto);
+
+  hotSpotDiv.text = "Ciao";
+  hotSpotDiv.onclick = function() {
+    window.location.href = "../page/Carlo.html";
   };
   hotSpotDiv.onmouseenter = function() {
     nuovoOggetto.style.display = "Block";

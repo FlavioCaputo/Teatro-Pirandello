33f43cba545b56eaaf5ac9d3a0f4f57aa252ec1f
FlavioCaputo <161652421+FlavioCaputo@users.noreply.github.com>
2024-10-04 18:50:48 +0200

    Update Mappa.js

--- Static/js/Mappa.js
+++ Static/js/Mappa.js
@@ -1,14 +1,8 @@
 var overlay = document.getElementById('videobg');
 var mobov = document.getElementById('co');
-var t1 = document.getElementById('foyer')
-var t2 = document.getElementById('stanza1')
 var b1 = document.getElementById('bot')
 var b2 = document.getElementById('bot2')
 document.body.style.overflow = "hidden";
-t1.classList.add('fade-out')
-t1.style.display = 'none';
-t2.classList.add('fade-out')
-t2.style.display = 'none';
 b1.classList.add('fade-out')
 b2.classList.add('fade-out')
 overlay.style.display = 'none';
@@ -35,21 +29,15 @@ function visita2() {
     window.location.href = "Visita2.html";
 }
 function Posin() {
-    t1.style.display = 'block';
-    t1.classList.remove('fade-out')
-    b1.classList.remove('fade-out')
+    b1.classList.remove('fade-out');
 }
 function Posout() {
-    t1.classList.add('fade-out')
-    b1.classList.add('fade-out')
+    b1.classList.add('fade-out');
 }
 
 function Posin2() {
-    t2.style.display = 'block';
-    t2.classList.remove('fade-out')
-    b2.classList.remove('fade-out')
+    b2.classList.remove('fade-out');
 }
 function Posout2() {
-    t2.classList.add('fade-out')
-    b2.classList.add('fade-out')
+    b2.classList.add('fade-out');
 }

1172d9b9d30cc54a759c5b0a0940e892111d4e55
FlavioCaputo <161652421+FlavioCaputo@users.noreply.github.com>
2024-10-12 19:28:54 +0200

    Update Mappa.js

--- Static/js/Mappa.js
+++ Static/js/Mappa.js
@@ -9,8 +9,12 @@ b2.classList.add('fade-out');
 b3.classList.add('fade-out');
 mobov.style.display = 'block';
 const view = window.innerWidth;
+var selectedButton = null; // Variabile per memorizzare l'elemento selezionato 
 if (view > 738) {
     handleResize()
+}
+function isMobile() {
+    return 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
 }
 function handleResize() {
     console.log('mag');
@@ -23,13 +27,35 @@ function rit() {
     window.location.href = "../../Home.html";
 }
 function visita() {
-    window.location.href = "Visita.html";
+    if (isMobile()) {
+        if (selectedButton == b1){
+            window.location.href = "Visita.html";
+        }
+        selectedButton = b1;
+    }else {
+        window.location.href = "Visita.html";
+    }
 }
 function visita2() {
-    window.location.href = "Visita2.html";
+    if (isMobile()) {
+        if (selectedButton ==b2){
+            window.location.href = "Visita2.html";
+        }
+        selectedButton = b2;
+    }else {
+        window.location.href = "Visita2.html";
+    }
 }
 function visita3() {
-    window.location.href = "Visita3.html";
+    if (isMobile()) {
+        if (selectedButton == b3){
+            window.location.href = "Visita3.html";
+            selectedButton = null;
+        }
+        selectedButton = b3;
+    }else {
+        window.location.href = "Visita3.html";
+    }
 }
 function Posin() {
     b1.classList.remove('fade-out');

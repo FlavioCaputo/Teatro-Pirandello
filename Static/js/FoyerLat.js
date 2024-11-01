b31bd58f2d4924e6c50ba4c644d0ab3c7dc3fa89
FlavioCaputo <161652421+FlavioCaputo@users.noreply.github.com>
2024-11-01 14:28:30 +0100

    Update FoyerLat.js

--- Static/js/FoyerLat.js
+++ Static/js/FoyerLat.js
@@ -1,6 +1,6 @@
 pannellum.viewer('panorama', {
     "type": "equirectangular",
-    "panorama": "https://flaviocaputo.github.io/Teatro-Pirandello/Static/Panorama/Stanza.jpg",
+    "panorama": "../Panorama/Stanza.jpg",
     "autoLoad": true,
     "showControls": false,
     "hotSpots": [

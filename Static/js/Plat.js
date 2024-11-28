061977bdad19d9dcc20114d2f513b14e1ef119ee
FlavioCaputo <161652421+FlavioCaputo@users.noreply.github.com>
2024-11-28 23:31:07 +0100

    Add files via upload

---
+++ Static/js/Plat.js
@@ -0,0 +1,28 @@
+pannellum.viewer('panorama', {
+  "type": "equirectangular",
+  "panorama": "https://raw.githubusercontent.com/FlavioCaputo/Teatro-Pirandello/refs/heads/main/Static/Panorama/teatro.jpeg",
+  "autoLoad": true,
+  "showControls": false,
+  "yaw": 90, 
+  "hotSpots": [
+    {
+      "pitch": -8,
+      "yaw": -90,
+      "cssClass": "custom-hotspot2",
+      "text": "Stanza Laterale",
+      "createTooltipFunc": Freccia1,
+      "createTooltipArgs": "Custom Hotspot"
+    }
+]
+})
+
+function rit() {
+  window.location.href = "../page/Mappa.html";
+}
+
+function Freccia1(hotSpotDiv, args) {
+  hotSpotDiv.classList.add('custom-hotspot');
+  hotSpotDiv.onclick = function() {
+      window.location.href = "../page/Visita3.html";
+    };
+}
